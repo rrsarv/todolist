913c09aa3958ffa85791e4994fc56580a981e111
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-02 18:30:19 +0300

    Update header-component.js

--- src/view/header-component.js
+++ src/view/header-component.js
@@ -1,4 +1,4 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 
 
 function createHeaderComponentTemplate() {
@@ -12,23 +12,15 @@ function createHeaderComponentTemplate() {
 }
 
 
-export default class HeaderComponent {
-  getTemplate() {
+export default class HeaderComponent extends AbstractComponent {
+  constructor() {
+    super();
+  }
+  
+  get template() {
     return createHeaderComponentTemplate();
   }
 
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
 
-
-    return this.element;
-  }
-
-
-  removeElement() {
-    this.element = null;
-  }
 }
