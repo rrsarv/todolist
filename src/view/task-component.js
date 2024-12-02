3e041a2c647ea70a96a8468485388206435c6d70
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-02 18:29:11 +0300

    Update task-component.js

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,40 +1,30 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 
 
 function createTaskComponentTemplate(task) {
-    const {title, status} = task;
-        return (
-            `<ul class="list_${status}"> 
-        <li class="task">${title}</li> 
-        </ul>`
+  const{status,title}=task;
+  return (
+`        
+        <li class="task--${status}">${title}</li>
+        `
         );
 
 }
 
-export default class TaskComponent {
+ 
+  
+
+  export default class TaskComponent extends AbstractComponent {
 
     constructor ({task}) {
+      super();
         this.task = task;
     }
 
-    getTemplate() {
+    get template() {
         return createTaskComponentTemplate (this.task);
     }
 
-
-    getElement() {
-        if (!this.element) {
-          this.element = createElement(this.getTemplate());
-        }
-    
-    
-        return this.element;
-      }
-    
-    
-      removeElement() {
-        this.element = null;
-      }
 
 }
 
