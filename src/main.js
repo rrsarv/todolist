2a9f47597c4389d605c1ad87ede996684ea63813
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-02 18:27:47 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,25 +3,33 @@ import FormComponent from './view/form-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import {render, RenderPosition} from './framework/render.js';
 import TasksModel from './model/task-model.js';
+import FormAddTaskComponent from './view/form-component.js';
 
 
 
 const bodyContainer= document.querySelector('.header');
 const formContainer= document.querySelector('.new_task');
- const tasksBoardContainer = document.querySelector('.row');
+const tasksBoardContainer = document.querySelector('.row');
 
 
  const tasksModel = new TasksModel();
+ const headerComponent = new HeaderComponent();
  const tasksBoardPresenter = new TasksBoardPresenter({
       boardContainer: tasksBoardContainer,
       tasksModel,
      });
    
+const formAddTaskComponent = new FormAddTaskComponent({
+      onClick: handleNewTaskButtonClick,
+ });
+      
+function handleNewTaskButtonClick() {
+          tasksBoardPresenter.createTask();
+}
 
 
-
-render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
-render(new FormComponent(), formContainer);
+render(headerComponent, bodyContainer, RenderPosition.AFTERBEGIN);
+render(formAddTaskComponent, formContainer, RenderPosition.AFTERBEGIN);
 
 
 tasksBoardPresenter.init();
