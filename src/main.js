f1b268a2d93da7f2f6656a541ba99e6588950505
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-02 17:46:17 +0300

    Update main.js
    add code

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 import HeaderComponent from './view/header-component.js';
 import FormComponent from './view/form-component.js';
-import BacklogComponent from './view/backlog-component.js';
-import DoneComponent from './view/done-component.js';
-import InProgressComponent from './view/in_progress-component.js';
-import GarbageComponent from './view/garbage-component.js';
+import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import {render, RenderPosition} from './framework/render.js';
+import TasksModel from './model/task-model.js';
+
 
 
 const bodyContainer= document.querySelector('.header');
 const formContainer= document.querySelector('.new_task');
-const backlogContainer= document.querySelector('.backlog');
-const inProgressContainer= document.querySelector('.in_progress');
-const doneContainer= document.querySelector('.done');
-const garbageContainer= document.querySelector('.garbage');
+ const tasksBoardContainer = document.querySelector('.row');
+
+
+ const tasksModel = new TasksModel();
+ const tasksBoardPresenter = new TasksBoardPresenter({
+      boardContainer: tasksBoardContainer,
+      tasksModel,
+     });
+   
 
 
 
-render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
-render(new FormComponent(), formContainer, RenderPosition.AFTERBEGIN);
-render(new BacklogComponent(), backlogContainer, RenderPosition.AFTERBEGIN);
-render(new InProgressComponent(), inProgressContainer, RenderPosition.AFTERBEGIN);
-render(new DoneComponent(), doneContainer, RenderPosition.AFTERBEGIN);
-render(new GarbageComponent(), garbageContainer, RenderPosition.AFTERBEGIN);
+render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
+render(new FormComponent(), formContainer);
+
+
+tasksBoardPresenter.init();
