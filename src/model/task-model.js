e8d95e065e028c2a695d6e2f2e1d3cd1f30db001
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-02 18:32:01 +0300

    Update task-model.js

--- src/model/task-model.js
+++ src/model/task-model.js
@@ -1,13 +1,41 @@
+import { generateId } from '../utils.js';
 import { tasks } from '../mock/task.js';
 
-
-
-
 export default class TasksModel {
- boardtasks = tasks;
+ #boardtasks = tasks;
+ #observers =[];
 
+ get tasks() {
+   return this.#boardtasks;
+ }
+ getTasksByStatus(status){
+  return this.#boardtasks.filter(task=>task.status===status);
+ }
 
- getTasks() {
-   return this.boardtasks;
+ addTask(title){
+  const newTask = {
+    id: generateId(),
+    title,
+    status:'backlog',
+  };
+
+  this.#boardtasks.push(newTask);
+  this._notifyObservers();
+  return newTask;
+ }
+ clearTasksByStatus(status) {
+  this.#boardtasks = this.#boardtasks.filter(task => task.status !== status);
+}
+
+ addObserver(observer){
+  this.#observers.push(observer);
+ }
+
+ removeObserver(observer){
+  this.#observers=this.#observers.filter((obs) => obs !== observer);
+ }
+
+ _notifyObservers(){
+  this.#observers.forEach((observer) => observer());
  }
 }
