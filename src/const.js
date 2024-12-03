7f6b7d35cf8517421ca9c5aa707627bcdf4d952f
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-03 20:40:37 +0300

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,15 +1,29 @@
 export const Status = { 
-    BACKLOG: `backlog`, 
-    PROCESSING: `in_progress`, 
-    DONE: `done`, 
-    BASKET: `garbage`, 
-  }; 
-   
+  BACKLOG: `backlog`, 
+  PROCESSING: `in_progress`, 
+  DONE: `done`, 
+  BASKET: `garbage`, 
+}; 
  
- 
+
+
 export const StatusLabel = { 
-    [Status.BACKLOG]:` Бэклог`, 
-    [Status.PROCESSING]: `В процессе`, 
-    [Status.DONE]:` Готово`, 
-    [Status.BASKET]:` Корзина`, 
-  };
+  [Status.BACKLOG]:` Бэклог`, 
+  [Status.PROCESSING]: `В процессе`, 
+  [Status.DONE]:` Готово`, 
+  [Status.BASKET]:` Корзина`, 
+};
+
+
+export const  UserAction ={
+  UPDATE_TASK: 'UPDATE_TASK',
+  ADD_TASK: 'ADD_TASK',
+  DELETE_TASK: 'DELETE_TASK',
+};
+
+export const  UpdateType ={
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT',
+};
