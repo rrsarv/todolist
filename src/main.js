36304a6a842018898537c90f56bfee7de0594a82
rrsarv <164464541+rrsarv@users.noreply.github.com>
2024-12-03 20:40:16 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import HeaderComponent from './view/header-component.js';
-import FormComponent from './view/form-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import {render, RenderPosition} from './framework/render.js';
 import TasksModel from './model/task-model.js';
 import FormAddTaskComponent from './view/form-component.js';
+import TasksApiService from './tasks-api-service.js';
 
 
-
+const END_POINT ='https://673b5583339a4ce4451bca0a.mockapi.io'
 const bodyContainer= document.querySelector('.header');
 const formContainer= document.querySelector('.new_task');
 const tasksBoardContainer = document.querySelector('.row');
 
 
- const tasksModel = new TasksModel();
+const tasksModel = new TasksModel({
+     tasksApiService: new TasksApiService(END_POINT)
+   });
  const headerComponent = new HeaderComponent();
  const tasksBoardPresenter = new TasksBoardPresenter({
       boardContainer: tasksBoardContainer,
